edffe63c9eca61915f8c6ab137007ec2bee6b226
Tom Warner <tommwarner@gmail.com>
2023-08-03 19:51:09 -0400

    added isAdmin boolean to User table

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,10 +1,8 @@
 const Sequelize = require('sequelize');
-const config = {
-};
 
-if(process.env.QUIET){
-  config.logging = false;
-}
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_shopping_db', config);
+const conn = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/acme_shopping_db',
+  process.env.QUIET || { logging: false }
+);
 
 module.exports = conn;
