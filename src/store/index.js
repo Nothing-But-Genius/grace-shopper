fa7c663e4e55247d760cf99f2d45b93f35054c60
AlfredoFerreiras <114266168+AlfredoFerreiras@users.noreply.github.com>
2023-08-09 09:54:02 -0400

    added changes to the local api and created components to display user and create user, but they still have bugs

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,21 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import auth from './auth';
-import cart from './cart';
-import productsReducer from './product';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import auth from "./auth";
+import cart from "./cart";
+import productsReducer from "./product";
+import userReducer from "./user";
 
 const reducer = combineReducers({
   auth,
   cart,
   products: productsReducer,
+  users: userReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
 
-export * from './auth';
-export * from './cart';
-export * from './product';
+export * from "./auth";
+export * from "./cart";
